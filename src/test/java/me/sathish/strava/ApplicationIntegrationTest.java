928766e16ba23d412576f87a89094ea6604b1a66
Sathish Jayapal <sjayapal@skminfotech.com>
2024-05-11 11:01:43 -0500

    stravaruns ms

---
+++ src/test/java/me/sathish/strava/ApplicationIntegrationTest.java
@@ -0,0 +1,10 @@
+package me.sathish.strava;
+
+import me.sathish.strava.common.AbstractIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+class ApplicationIntegrationTest extends AbstractIntegrationTest {
+
+    @Test
+    void contextLoads() {}
+}
